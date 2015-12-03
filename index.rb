e64675be1dc44a9306dc047089b9e6c21ca1cd92
itgsod-sandro-caputo <sandro.caputo@elev.it-gymnasiet.se>
2015-12-03 14:55:27 +0100

    Playing with some drop-down menus & Added error pages & rescues

--- index.rb
+++ index.rb
@@ -3,17 +3,17 @@ require 'dm-core'
 require 'dm-timestamps'
 require 'dm-validations'
 require 'dm-migrations'
-require 'dm-postgres-adapter'
-# require 'dm-sqlite-adapter'
+#require 'dm-postgres-adapter'
+require 'dm-sqlite-adapter'
 require 'bcrypt'
-# require 'sinatra/reloader'
+require 'sinatra/reloader'
 
 
 #https://git.heroku.com/stark-earth-2441.git
 set :port, 4568
 
-# DataMapper.setup :default, "sqlite://#{Dir.pwd}/database.db"
-DataMapper.setup(:default, 'postgres://uuzfqirtsdlqch:Qngf4-VL2xom7pTmiBwaZH6L6f@ec2-54-217-240-205.eu-west-1.compute.amazonaws.com/d48tmpto2mh5fi')
+ DataMapper.setup :default, "sqlite://#{Dir.pwd}/database.db"
+#DataMapper.setup(:default, 'postgres://uuzfqirtsdlqch:Qngf4-VL2xom7pTmiBwaZH6L6f@ec2-54-217-240-205.eu-west-1.compute.amazonaws.com/d48tmpto2mh5fi')
 set :static, true
 set :public_folder, 'public'
 
@@ -29,11 +29,12 @@ class User
   property :updated_at , DateTime
 end
 
-class Messages
+class Events
   include DataMapper::Resource
 
   property :id          , Serial
-  property :message     , String
+  property :event_type  , String
+  property :event_time  , String
   property :user        , String
 end
 
@@ -44,10 +45,6 @@ class Friends
   property :user1       , String
   property :user2       , String
 end
-
-#
-# ett table med person 1 och person 2 = friends
-# else if p1 = jnkjn and p2 = nil ....
 
 configure :development do
   DataMapper.auto_upgrade!
@@ -72,30 +69,32 @@ helpers do
   end
 end
 
-####################################
+not_found do
+  status 404
+  erb :error_404
+end
 
 get '*/private' do
     unless authorized?
         halt(401, 'Unauthorized')
     end
+    begin
     @name = User.get(User.first(username: session[:username]).id).username
-    #@time = Messages.all(user: session[:username]).created_at
 
-    @messages = Messages.all(user: session[:username])
+    #@friend_msg = Messages.all(user: Friends.first(user1: session[:username]).user2)
+    #p @friend_msg
 
-    @friend_msg = Messages.all(user: Friends.first(user1: session[:username]).user2)
-    p @friend_msg
-
-    @times = [12, 132, 1433, 15554]
-  erb :test
+    #@times = [12, 132, 1433, 15554]
+    rescue
+      erb :error_500
+    end
+  erb :main_page
 end
 
-post '/add_message' do
-  Messages.create(message: params['message'], user: session[:username])
+post '/create_event' do
+  Events.create(event_type: params['event_type'], event_time: params['event_time'] , user: session[:username])
   redirect to('/private')
 end
-
-#####################################
 
 get '*/login' do
   session[:username] = nil
@@ -108,52 +107,51 @@ get '*/logout' do
 end
 
 post '/login' do
-  if User.first(username: params['username']) != nil
-  id = User.first(username: params['username']).id
-  if User.get(id).password == BCrypt::Engine.hash_secret(params['password'], User.get(id).salt)
-    session[:username] = User.get(id).username
+  begin
+    if User.first(username: params['username']) != nil
+    id = User.first(username: params['username']).id
+    if User.get(id).password == BCrypt::Engine.hash_secret(params['password'], User.get(id).salt)
+      session[:username] = User.get(id).username
+    end
+    else
+      halt(401, 'Invalid login')
+    end
+    redirect to('/private')
+  rescue
+    erb :error_500
   end
-    else halt(401, 'Invalid login')
-  end
-  redirect to('/private')
 end
 
 post '/signup' do
-  password_salt = BCrypt::Engine.generate_salt
-  password_hash = BCrypt::Engine.hash_secret(params['password'], password_salt)
-  username = params['username']
-  user = User.new username: username, password: password_hash, salt: password_salt
-  user.save
-
-  redirect to('/login')
+  begin
+    password_salt = BCrypt::Engine.generate_salt
+    password_hash = BCrypt::Engine.hash_secret(params['password'], password_salt)
+    username = params['username']
+    user = User.new username: username, password: password_hash, salt: password_salt
+    user.save
+    redirect to('/login')
+  rescue
+    erb :error_500
+  end
 end
-
-
 
 get '*/add_friend_test' do
   erb :add_friend_test
 end
 
 post '/add_friend' do
-  if User.first(username: params['add_friend']).username == params['add_friend']
-     Friends.create(user1: session[:username], user2: params['add_friend'])
+  begin
+    if User.first(username: params['add_friend']).username == params['add_friend']
+      Friends.create(user1: session[:username], user2: params['add_friend'])
+      halt(200, "Successfully added friend! You're not forever alone atm! :D")
+    end
+  rescue
+    erb :error_500
   end
-  halt(200, "Successfully added friend! You're not forever alone atm! :D")
 end
-
-
 
 get '*/signup' do
   erb :signup
 end
 
 
-  # dynamic_name = "ClassName"
-  # Object.const_set(dynamic_name, Class.new {
-  #                                include DataMapper::Resource
-  #                                property :id , Serial
-  #                                property :friend , String
-  #                              })
-  #
-  # klass = Object.const_set name, Struct.new(*attributes)
-  #
--- testie.rb
+++ testie.rb
@@ -1,19 +1,20 @@
-def whatever()
-  3.times do
-    puts "1 at #{Time.now}"
-    sleep(2)
-  end
-end
+# def whatever()
+#   3.times do
+#     puts "1 at #{Time.now}"
+#     sleep(2)
+#   end
+# end
+#
+# def whatever2()
+#   3.times do
+#     puts "2 at #{Time.now}"
+#     sleep(1)
+#   end
+# end
+#
+# t1 = Thread.new {whatever}
+# t2 = Thread.new {whatever2}
+#
+# t1.join
+# t2.join
 
-def whatever2()
-  3.times do
-    puts "2 at #{Time.now}"
-    sleep(1)
-  end
-end
-
-t1 = Thread.new {whatever}
-t2 = Thread.new {whatever2}
-
-t1.join
-t2.join
