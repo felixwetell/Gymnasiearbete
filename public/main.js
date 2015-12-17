7a8d5d45f8e721f25479273c2e288dbc5b46c6c9
itgsod-sandro-caputo <sandro.caputo@elev.it-gymnasiet.se>
2015-12-17 13:38:37 +0100

    js code now in main.js instead of html file. + Minor optimisations

--- public/main.js
+++ public/main.js
@@ -1,3 +1,27 @@
-/**
- * Created by felix.wetell on 17/12/15.
- */
+function dateR(wday) {
+    var day = "";
+    switch (wday) {
+        case 1:
+            day = "Måndag";
+            break;
+        case 2:
+            day = "Tisdag";
+            break;
+        case 3:
+            day = "Onsdag";
+            break;
+        case 4:
+            day = "Torsdag";
+            break;
+        case 5:
+            day = "Fredag";
+            break;
+        case 6:
+            day = "Lördag";
+            break;
+        case 0:
+            day = "Söndag";
+            break;
+    }
+    return day
+}
